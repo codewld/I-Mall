936c506f52644ab75d581f617414a1c25c827b2c
codewld <wang_longda@outlook.com>
2022-02-03 21:29:57 +0800

    fix[Admin]: 修改了Tailwind CSS配置中的过时选项

--- I-Mall-Admin/tailwind.config.js
+++ I-Mall-Admin/tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: [],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}'
+  ],
   theme: {
     extend: {}
   },
