0160d36d48033f23d860d04e1d64713029359dac
codewld <wang_longda@outlook.com>
2022-02-04 14:56:42 +0800

    init[Server]

---
+++ I-Mall-Server/src/main/java/pers/codewld/imall/IMallApplication.java
@@ -0,0 +1,13 @@
+package pers.codewld.imall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IMallApplication.class, args);
+    }
+
+}
---
+++ I-Mall-Server/src/test/java/pers/codewld/imall/IMallApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.codewld.imall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
