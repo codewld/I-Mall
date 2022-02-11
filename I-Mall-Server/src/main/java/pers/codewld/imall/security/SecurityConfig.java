dc8a75e4c8fa558985968e81c956654c29abf892
codewld <wang_longda@outlook.com>
2022-02-11 17:35:44 +0800

    fix[Server]: 去除Spring Security默认生成的密码

--- I-Mall-Server/src/main/java/pers/codewld/imall/security/SecurityConfig.java
+++ I-Mall-Server/src/main/java/pers/codewld/imall/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package pers.codewld.imall.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -32,6 +33,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     JWTVerifyFilter JWTVerifyFilter;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // 该语句的作用是：去除默认生成的密码
+        auth.inMemoryAuthentication();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
