733b5366a4aa1cde4e83985b7b94c0485748c865
codewld <wang_longda@outlook.com>
2022-02-14 17:04:02 +0800

    feat[Server]: 统一响应的contentType

---
+++ I-Mall-Server/src/main/java/pers/codewld/imall/config/WebConfiguration.java
@@ -0,0 +1,16 @@
+package pers.codewld.imall.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
+
+}
