dcb7e25946b3384c16208fa49c822ca51268034b
codewld <wang_longda@outlook.com>
2022-02-14 17:40:56 +0800

    docs[Server]: 增加注释

--- I-Mall-Server/src/main/java/pers/codewld/imall/config/WebConfiguration.java
+++ I-Mall-Server/src/main/java/pers/codewld/imall/config/WebConfiguration.java
@@ -5,9 +5,20 @@ import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * <p>
+ * Web 配置类
+ * </p>
+ *
+ * @author codewld
+ * @since 2022-02-14
+ */
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
+    /**
+     * 统一响应的contentType
+     */
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.defaultContentType(MediaType.APPLICATION_JSON);
