96be73219095accb72efe85e1c2b441cc7f19a4c
codewld <wang_longda@outlook.com>
2022-02-15 19:49:05 +0800

    feat[Server]: 配置后端跨域

--- I-Mall-Server/src/main/java/pers/codewld/imall/config/WebConfiguration.java
+++ I-Mall-Server/src/main/java/pers/codewld/imall/config/WebConfiguration.java
@@ -3,6 +3,7 @@ package pers.codewld.imall.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -22,6 +23,25 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
+
+    /**
+     * 跨域
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //添加映射路径
+        registry.addMapping("/**")
+                //是否发送Cookie
+                .allowCredentials(true)
+                //设置放行哪些原始域
+                .allowedOriginPatterns("http://localhost:[*]")
+                //放行哪些请求方式
+                .allowedMethods("*")
+                //放行哪些原始请求头部信息
+                .allowedHeaders("*")
+                //暴露哪些原始请求头部信息
+                .exposedHeaders("*");
     }
 
 }
