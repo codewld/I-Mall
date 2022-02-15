0eb5759b989244012a068d16a47308011c0adc63
codewld <wang_longda@outlook.com>
2022-02-15 20:43:45 +0800

    feat[Admin]: 集成axios

---
+++ I-Mall-Admin/src/api/request/index.ts
@@ -0,0 +1,69 @@
+import axios, { Method } from 'axios';
+import router from '@/router';
+import { ElMessage } from 'element-plus';
+import 'element-plus/es/components/message/style/css';
+import { statusCode, baseURL } from '@/config';
+import { unref } from 'vue';
+
+let instance = axios.create({
+  baseURL: baseURL,
+  timeout: 5000
+})
+
+// 请求拦截器
+instance.interceptors.request.use(
+  (config: any) => {
+    let token = 'jwt'
+    config.headers.Authorization = token
+    return config
+  }, () => {
+    return Promise.reject('网络错误')
+  }
+)
+
+// 响应拦截器
+instance.interceptors.response.use(
+  res => {
+    // 成功
+    if (res.data.code === statusCode.SUCCESS) {
+      return res.data.data
+    }
+    // 未登录
+    if (res.data.code == statusCode.UNAUTHORIZED) {
+      ElMessage.error('身份验证失败，跳转至登录页')
+      return new Promise((resolve, reject) => {
+        router.replace('login').then(() => {
+          reject('请重新登录')
+        })
+      })
+    }
+    // 未授权
+    if (res.data.code == statusCode.FORBIDDEN) {
+
+      // todo 刷新权限
+
+      return Promise.reject(res.data.msg)
+    }
+    // 其它错误
+    return Promise.reject(res.data.msg)
+  }, () => {
+    return Promise.reject('响应错误')
+  }
+)
+
+/**
+ * 发送网络请求
+ * @param path 请求地址后缀
+ * @param method HTTP 方法
+ * @param data 传递的数据
+ * @return Promise
+ */
+const request = <T>(path: string, method: Method = 'get', data ?: object): Promise<T> => {
+  return instance.request({
+    url: path,
+    method: method,
+    data: unref(data)
+  })
+}
+
+export default request
