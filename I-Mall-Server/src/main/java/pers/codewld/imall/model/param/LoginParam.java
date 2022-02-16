6e7c98f6cee62f9956a60a3e8e691716dff1b857
codewld <wang_longda@outlook.com>
2022-02-16 14:14:17 +0800

    fix[Server]: 修复登录密码为空时无法正确校验的错误

--- I-Mall-Server/src/main/java/pers/codewld/imall/model/param/LoginParam.java
+++ I-Mall-Server/src/main/java/pers/codewld/imall/model/param/LoginParam.java
@@ -4,7 +4,7 @@ import io.swagger.annotations.ApiModel;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 /**
@@ -23,7 +23,7 @@ public class LoginParam {
     @Size(min = 5, max = 10, message = "用户名长度应在5-10之间")
     private String username;
 
-    @NotNull(message = "密码不能为空")
+    @NotBlank(message = "密码不能为空")
     private String password;
 
 }
