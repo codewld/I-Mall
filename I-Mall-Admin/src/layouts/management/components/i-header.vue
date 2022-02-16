609d7aa43d7c64b435b1151814f478f16562eea5
codewld <wang_longda@outlook.com>
2022-02-16 23:13:42 +0800

    feat[Admin]: 增加登出功能

--- I-Mall-Admin/src/layouts/management/components/i-header.vue
+++ I-Mall-Admin/src/layouts/management/components/i-header.vue
@@ -1,13 +1,51 @@
 <script setup lang="ts">
+import { useJWTStore } from '@/store';
+import { ElMessage, ElMessageBox } from 'element-plus';
+import 'element-plus/es/components/message/style/css';
+import 'element-plus/es/components/message-box/style/css';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+/**
+ * 退出登录确认
+ */
+const handleLogout = () => {
+  ElMessageBox.confirm(
+      '是否要退出登录?',
+      '提醒',
+      {
+        confirmButtonText: '确认',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }
+  )
+      .then(() => {
+        logout()
+      })
+      .catch(() => {
+      })
+}
+
+/**
+ * 退出登录
+ */
+const logout = () => {
+  const JWTStore = useJWTStore()
+  JWTStore.reset()
+  ElMessage.success('退出成功')
+  router.replace('login')
+}
 </script>
 
 <template>
-  <div class="h-full px-6 box-border z-10 flex justify-between items-center border-0 border-b border-gray-300 border-solid">
+  <div
+      class="h-full px-6 box-border z-10 flex justify-between items-center border-0 border-b border-gray-300 border-solid">
     <h1 class="text-4xl tracking-wider">
       I-Mall
     </h1>
     <div>
-      <el-button type="text" class="text-red-500">退出登录</el-button>
+      <el-button type="text" class="text-red-500" @click="handleLogout">退出登录</el-button>
     </div>
   </div>
 </template>
