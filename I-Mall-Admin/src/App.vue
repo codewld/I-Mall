e426a3a7a657b68aad81ff4cedfc0333d2a88080
codewld <wang_longda@outlook.com>
2022-02-17 16:37:32 +0800

    feat[Admin]: 配置Element Plus国际化，全局配置为中文

--- I-Mall-Admin/src/App.vue
+++ I-Mall-Admin/src/App.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+
+const locale = zhCn
+</script>
+
 <template>
-  <router-view/>
+  <el-config-provider :locale="locale">
+    <router-view/>
+  </el-config-provider>
 </template>
