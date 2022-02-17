fce15838b53bcfacadaebe5447a13386dbf301e9
codewld <wang_longda@outlook.com>
2022-02-17 16:00:24 +0800

    feat[Admin]: 增加布局容器

---
+++ I-Mall-Admin/src/components/iContainer/i-container.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <el-scrollbar>
+    <el-space direction="vertical" fill>
+      <slot></slot>
+    </el-space>
+  </el-scrollbar>
+</template>
+
+<style scoped>
+/deep/ .el-space__item, .el-space {
+  width: 100%;
+}
+</style>
---
+++ I-Mall-Admin/src/components/iContainer/index.ts
@@ -0,0 +1,9 @@
+/**
+ * 容器组件
+ *
+ * 集成了el-scrollbar和el-space，适用于布局多个垂直块元素
+ */
+
+import iContainer from './i-container.vue'
+
+export default iContainer
