deaff2a29c88156ab1868912c3618439168bb35e
codewld <wang_longda@outlook.com>
2022-02-21 23:15:12 +0800

    refactor[Admin]: 完善Ums-Admin类

--- I-Mall-Admin/src/views/ums/admin/index.vue
+++ I-Mall-Admin/src/views/ums/admin/index.vue
@@ -6,23 +6,27 @@ import ICurd from '@/components/iCurd';
 import { CURD } from '@/@types/curd';
 
 const fieldList: CURD.field[] = [
-  { code: 'id', name: 'id', tableConf: { width: 80, fixed: 'left' }, formConf: { add: false, readOnly: true } },
-  { code: 'username', name: '用户名', tableConf: { width: 200 } },
+  { code: 'id', name: 'id', tableConf: { width: 80, fixed: 'left' }, formConf: { add: false, update: false } },
+  { code: 'username', name: '用户名', tableConf: { width: 200 }, searchConf: { display: true } },
   { code: 'password', name: '密码', tableConf: { display: false }, formConf: { see: false } },
   { code: 'icon', name: '头像', tableConf: { display: false } },
-  { code: 'email', name: '邮箱', tableConf: { width: 200 } },
-  { code: 'nickName', name: '昵称', tableConf: { width: 200 } },
+  { code: 'email', name: '邮箱', tableConf: { width: 200 }, searchConf: { display: true } },
+  { code: 'nickName', name: '昵称', tableConf: { width: 200 }, searchConf: { display: true } },
   { code: 'note', name: '备注', tableConf: { minWidth: 200 } },
-  { code: 'createTime', name: '创建时间', tableConf: { display: false }, formConf: { add: false, readOnly: true } },
-  { code: 'updateTime', name: '更新时间', tableConf: { display: false }, formConf: { add: false, readOnly: true } },
-  { code: 'loginTime', name: '最后登录时间', tableConf: { display: false }, formConf: { add: false, readOnly: true } },
-  { code: 'status', name: '启用状态', tableConf: { width: 80, fixed: 'right' } }
+  { code: 'createTime', name: '创建时间', tableConf: { display: false }, formConf: { add: false, update: false } },
+  { code: 'updateTime', name: '更新时间', tableConf: { display: false }, formConf: { add: false, update: false } },
+  { code: 'loginTime', name: '最后登录时间', tableConf: { display: false }, formConf: { add: false, update: false } },
+  { code: 'status', name: '启用状态', tableConf: { width: 80, fixed: 'right' }, searchConf: { display: true } }
 ]
 </script>
 
 <template>
   <i-curd :add-function="rAdd" :del-function="rDel" :update-function="rUpdate" :load-function="rList"
           :field-list="fieldList">
+    <!--自定义搜索-->
+    <template v-slot:search-item-status="scope">
+      <el-switch v-model="scope.row.status" :disabled="scope.disabled"/>
+    </template>
     <!--自定义表格列-->
     <template v-slot:table-column-status="scope">
       <div class="flex items-center justify-center">
