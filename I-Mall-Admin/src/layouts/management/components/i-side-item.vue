dcfa1f7d7dcdc1144a2502d77bfd307981f3b6e0
codewld <wang_longda@outlook.com>
2022-03-04 18:43:18 +0800

    feat[Admin]: 增加侧边栏元素组件; 实现动态侧边栏

---
+++ I-Mall-Admin/src/layouts/management/components/i-side-item.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+import { PropType } from 'vue';
+
+const props = defineProps({
+  data: {
+    type: Object as PropType<Account.router>,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <template v-if="data.children !== undefined">
+    <el-sub-menu :index="data.path">
+      <template v-slot:title>
+        {{ data.name }}
+      </template>
+      <template v-for="(item, key) in data.children" :key="key">
+        <i-side-item :data="item"></i-side-item>
+      </template>
+    </el-sub-menu>
+  </template>
+  <template v-else>
+    <el-menu-item :index="data.path">
+      {{ data.name }}
+    </el-menu-item>
+  </template>
+</template>
--- I-Mall-Admin/src/layouts/management/components/i-side.vue
+++ I-Mall-Admin/src/layouts/management/components/i-side.vue
@@ -1,24 +1,20 @@
 <script setup lang="ts">
-import { ElSubMenu } from 'element-plus';
 import { useRoute } from 'vue-router'
 import { computed } from 'vue';
+import { useRouterStore } from '@/store';
+import ISideItem from '@/layouts/management/components/i-side-item.vue';
 
+// 当前路由
 const route = useRoute()
 const activeRoute = computed(() => route.path)
+
+const routerStore = useRouterStore();
 </script>
 
 <template>
   <el-menu router :default-active="activeRoute" class="h-full select-none">
-    <el-menu-item index="/home">
-      首页
-    </el-menu-item>
-    <el-sub-menu index="/ums">
-      <template v-slot:title>
-        后台管理
-      </template>
-      <el-menu-item index="/ums/admin">管理员管理</el-menu-item>
-      <el-menu-item index="/ums/role">角色管理</el-menu-item>
-      <el-menu-item index="/ums/menu">菜单管理</el-menu-item>
-    </el-sub-menu>
+    <template v-for="(item, key) in routerStore.value" :key="key">
+      <i-side-item :data="item"></i-side-item>
+    </template>
   </el-menu>
 </template>
