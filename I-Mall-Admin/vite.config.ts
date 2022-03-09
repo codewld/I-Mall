5fa36352d4626135d38738fdbc91eaae3c1f56a8
codewld <wang_longda@outlook.com>
2022-03-09 21:55:55 +0800

    feat[Admin]: 增加对jsx的支持

--- I-Mall-Admin/vite.config.ts
+++ I-Mall-Admin/vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +16,9 @@ export default defineConfig({
     Components({
       dirs: ['element-plus'],
       resolvers: [ElementPlusResolver()]
+    }),
+    vueJsx({
+      // options are passed on to @vue/babel-plugin-jsx
     })
   ],
   resolve: {
