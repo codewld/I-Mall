9ef5b64592cd890acfd27ee453554caa910fe654
codewld <wang_longda@outlook.com>
2022-03-10 17:14:24 +0800

    feat[Server]: 为Swagger增加认证支持

--- I-Mall-Server/src/main/java/pers/codewld/imall/config/SwaggerConfig.java
+++ I-Mall-Server/src/main/java/pers/codewld/imall/config/SwaggerConfig.java
@@ -6,9 +6,13 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -22,13 +26,15 @@ import springfox.documentation.spring.web.plugins.Docket;
 @EnableOpenApi
 public class SwaggerConfig {
     @Bean
-    public Docket createRestApi(){
+    public Docket createRestApi() {
         return new Docket(DocumentationType.OAS_30)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("pers.codewld.imall.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts());
     }
 
     private ApiInfo apiInfo() {
@@ -36,5 +42,30 @@ public class SwaggerConfig {
                 .title("I-Mall")
                 .description("I-Mall接口文档")
                 .build();
+    }
+
+    private List<SecurityScheme> securitySchemes() {
+        List<SecurityScheme> list = new ArrayList<>();
+        ApiKey apiKey = new ApiKey("Authorization", "Authorization", "header");
+        list.add(apiKey);
+        return list;
+    }
+
+    private List<SecurityContext> securityContexts() {
+        List<SecurityContext> list = new ArrayList<>();
+        list.add(SecurityContext
+                .builder()
+                .securityReferences(defaultAuth())
+                .operationSelector(o -> o.requestMappingPattern().matches("/.*"))
+                .build());
+        return list;
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        List<SecurityReference> list = new ArrayList<>();
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[]{authorizationScope};
+        list.add(new SecurityReference("Authorization", authorizationScopes));
+        return list;
     }
 }
