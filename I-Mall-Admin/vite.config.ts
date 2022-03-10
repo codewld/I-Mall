2c83ea845feda85dd5053d909dc567d89997b694
codewld <wang_longda@outlook.com>
2022-03-10 15:31:32 +0800

    refactor[Admin]: 引入vite插件，已解决首次打开页面时加载缓慢的问题

--- I-Mall-Admin/vite.config.ts
+++ I-Mall-Admin/vite.config.ts
@@ -5,6 +5,8 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import OptimizationPersist from 'vite-plugin-optimize-persist'
+import PkgConfig from 'vite-plugin-package-config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,7 +21,9 @@ export default defineConfig({
     }),
     vueJsx({
       // options are passed on to @vue/babel-plugin-jsx
-    })
+    }),
+    PkgConfig(),
+    OptimizationPersist()
   ],
   resolve: {
     alias: {
