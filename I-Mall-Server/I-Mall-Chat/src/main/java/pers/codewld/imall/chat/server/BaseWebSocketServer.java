60591795a3aac3a151720cf296e147e99d1d320b
codewld <wang_longda@outlook.com>
2022-03-25 20:52:24 +0800

    refactor[Server]: 优化代码

--- I-Mall-Server/I-Mall-Chat/src/main/java/pers/codewld/imall/chat/server/BaseWebSocketServer.java
+++ I-Mall-Server/I-Mall-Chat/src/main/java/pers/codewld/imall/chat/server/BaseWebSocketServer.java
@@ -2,13 +2,13 @@ package pers.codewld.imall.chat.server;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import pers.codewld.imall.chat.model.entity.User;
 import pers.codewld.imall.chat.model.enums.SystemCode;
 import pers.codewld.imall.chat.model.message.queue.CommunicationMsg;
 import pers.codewld.imall.chat.model.message.queue.UserStatusMsg;
+import pers.codewld.imall.chat.model.message.websocket.WebSocketMsg;
 import pers.codewld.imall.chat.util.ConfigUtil;
 import pers.codewld.imall.common.util.BeanUtil;
 import pers.codewld.imall.common.util.RedisUtil;
@@ -62,18 +62,6 @@ public abstract class BaseWebSocketServer {
     }
 
     /**
-     * JSON工具
-     */
-    ObjectMapper objectMapper;
-
-    public ObjectMapper getObjectMapper() {
-        if (objectMapper == null) {
-            objectMapper = new ObjectMapper();
-        }
-        return objectMapper;
-    }
-
-    /**
      * 处理前队列
      */
     private String getPreQueue() {
@@ -83,7 +71,7 @@ public abstract class BaseWebSocketServer {
     /**
      * 静态变量，记录所有的连接
      */
-    private static final Map<String, BaseWebSocketServer> connections = new LinkedHashMap<>();
+    public static final Map<String, BaseWebSocketServer> connections = new LinkedHashMap<>();
 
     /**
      * 与当前连接对应的Session
@@ -108,8 +96,7 @@ public abstract class BaseWebSocketServer {
         this.session = session;
         connections.put(String.valueOf(id), this);
         this.user = new User(getSystemCode(), id);
-        // 向消息队列中传递消息
-        getRedisUtil().lPush(getPreQueue(), new UserStatusMsg(user, true, false), 0);
+        sendMsgToPreQueue(new UserStatusMsg(user, true, false));
     }
 
     /**
@@ -117,27 +104,24 @@ public abstract class BaseWebSocketServer {
      */
     @OnMessage
     public void onMessage(String message) {
-        JSONObject jsonObject = JSONObject.parseObject(message);
-        JSONObject data = jsonObject.getJSONObject("data");
-        switch (jsonObject.getString("type")) {
+        WebSocketMsg webSocketMsg = JSONObject.parseObject(message, WebSocketMsg.class);
+        JSONObject data = JSONObject.parseObject(webSocketMsg.getData());
+        switch (webSocketMsg.getType()) {
             // 活跃状态
             case "activeStatus":
                 Boolean active = data.getBoolean("active");
                 contact = null;
-                // 传递信息至消息队列
-                getRedisUtil().lPush(getPreQueue(), new UserStatusMsg(user, true, active, null), 0);
+                sendMsgToPreQueue(new UserStatusMsg(user, true, active, null));
                 break;
             // 会话建立
             case "sessionEstablish":
                 contact = JSON.toJavaObject(data.getJSONObject("contact"), User.class);
-                // 传递信息至消息队列
-                getRedisUtil().lPush(getPreQueue(), new UserStatusMsg(user, true, true, contact), 0);
+                sendMsgToPreQueue(new UserStatusMsg(user, true, true, contact));
                 break;
             // 发送消息
             case "sendMsg":
                 String msg = data.getString("msg");
-                // 传递信息至消息队列
-                getRedisUtil().lPush(getPreQueue(), new CommunicationMsg(user, contact, msg, LocalDateTime.now()), 0);
+                sendMsgToPreQueue(new CommunicationMsg(user, contact, msg, LocalDateTime.now()));
                 break;
             default:
                 break;
@@ -151,7 +135,7 @@ public abstract class BaseWebSocketServer {
     public void onError(Throwable error) throws Throwable {
         log.error(error.getMessage());
         // 向消息队列中传递消息
-        getRedisUtil().lPush(getPreQueue(), new UserStatusMsg(user, false), 0);
+        sendMsgToPreQueue(new UserStatusMsg(user, false));
         throw error;
     }
 
@@ -161,8 +145,7 @@ public abstract class BaseWebSocketServer {
     @OnClose
     public void onClose(@PathParam("id") Long id) {
         connections.remove(String.valueOf(id));
-        // 向消息队列中传递消息
-        getRedisUtil().lPush(getPreQueue(), new UserStatusMsg(user, false), 0);
+        sendMsgToPreQueue(new UserStatusMsg(user, false));
     }
 
     /**
@@ -170,5 +153,12 @@ public abstract class BaseWebSocketServer {
      */
     public void sendMessage(String message) throws IOException {
         session.getBasicRemote().sendText(message);
+    }
+
+    /**
+     * 向处理前队列中发送消息
+     */
+    private void sendMsgToPreQueue(Object msg) {
+        getRedisUtil().lPush(getPreQueue(), msg, 0);
     }
 }
