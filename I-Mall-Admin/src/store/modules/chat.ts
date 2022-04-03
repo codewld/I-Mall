013cff21faf9f08fa1e9c79b1793dc8feb8547a5
codewld <wang_longda@outlook.com>
2022-04-03 22:19:57 +0800

    refactor[Admin]: 迁移stringifyPlus()、parsePlus(); 增加对象是否相同的校验方法

--- I-Mall-Admin/src/store/modules/chat.ts
+++ I-Mall-Admin/src/store/modules/chat.ts
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia';
 import { getUserStr } from '@/utils/chatUtil';
-import { parsePlus, stringifyPlus } from '@/utils/serializeUtil';
+import { parsePlus, stringifyPlus } from '@/utils/objUtil';
 
 interface chatState {
   /**
--- I-Mall-Admin/src/utils/objUtil.ts
+++ I-Mall-Admin/src/utils/objUtil.ts
@@ -33,3 +33,58 @@ export function getChange(newObj: KVObj, oldObj: KVObj) {
   }
   return data
 }
+
+/**
+ * 将对象序列化为字符串
+ * <br>
+ * JSON.stringify的升级版，扩展了对Map和Set的支持
+ */
+export function stringifyPlus(obj: object) {
+  return JSON.stringify(
+    obj,
+    (k, v) => {
+      if (v instanceof Map) {
+        return {
+          type: 'Map',
+          value: [...v]
+        }
+      }
+      if (v instanceof Set) {
+        return {
+          type: 'Set',
+          value: [...v]
+        }
+      }
+      return v
+    }
+  )
+}
+
+/**
+ * 将字符串反序列化为对象
+ * <br>
+ * JSON.parse的升级版，扩展了对Map和Set的支持
+ */
+export function parsePlus(str: string) {
+  return JSON.parse(
+    str,
+    (k, v) => {
+      if (v?.type === 'Map') {
+        return new Map(v.value)
+      }
+      if (v?.type === 'Set') {
+        return new Set(v.value)
+      }
+      return v
+    }
+  )
+}
+
+/**
+ * 判断两个对象是否相同
+ * <br>
+ * 通过stringifyPlus判断
+ */
+export function isSame(obj1: object, obj2: object) {
+  return stringifyPlus(obj1) === stringifyPlus(obj2)
+}
