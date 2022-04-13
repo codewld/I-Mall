b364a6b53fd6d9c1b9a50eb3ffb87213dbc85632
codewld <wang_longda@outlook.com>
2022-04-13 11:08:25 +0800

    feat[Admin]: 新增日期格式化方法

--- I-Mall-Admin/src/utils/dateUtil.ts
+++ I-Mall-Admin/src/utils/dateUtil.ts
@@ -20,3 +20,29 @@ export function getFormattedDateTime(dateTimeString: string): string {
   return `${date} ${time}`
 }
 
+/**
+ * 格式化日期
+ */
+export function format(date: Date, format: string): string {
+  const o: any = {
+    'M+': date.getMonth() + 1, // 月份
+    'd+': date.getDate(), // 日
+    'h+': date.getHours(), // 小时
+    'm+': date.getMinutes(), // 分
+    's+': date.getSeconds(), // 秒
+    'q+': Math.floor((date.getMonth() + 3) / 3), // 季度
+    S: date.getMilliseconds() // 毫秒
+  }
+
+  if (/(y+)/.test(format)) {
+    format = format.replace(RegExp.$1, (date.getFullYear() + '').slice(4 - RegExp.$1.length))
+  }
+
+  for (const k in o) {
+    if (new RegExp('(' + k + ')', 'i').test(format)) {
+      format = format.replace(RegExp.$1, RegExp.$1.length === 1 ? o[k] : ('00' + o[k]).slice(('' + o[k]).length))
+    }
+  }
+
+  return format
+}
